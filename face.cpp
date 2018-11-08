1cde297ec324574014848eb160d9c6d3823c828b
Harry Waugh <hw16471@my.bristol.ac.uk>
2018-11-08 10:17:29 +0000

    Changed headers

--- face.cpp
+++ face.cpp
@@ -12,8 +12,8 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <opencv/cv.hpp>
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
 using namespace cv;
