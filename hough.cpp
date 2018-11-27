0ad2bd509b9c89e167bc91ad420b5f6d82ea5476
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2018-11-27 10:28:40 +0000

    almost finished houghTransform()

--- hough.cpp
+++ hough.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <opencv/cv.hpp>
 #include <math.h>
+#include <iostream>
+
+#define sqr(x)((x)*(x))
 
 using namespace cv;
 
@@ -72,7 +75,7 @@ void normalize(Mat &M, Mat &out)  {
 void calc_gradient_mag(Mat &left, Mat &right, Mat &out) {
 
     out.create(left.size(), CV_8U);
-		Mat unNormalised; 
+		Mat unNormalised;
 		left.copyTo(unNormalised);
 
     for (int y = 0; y < left.rows; y++) {
@@ -80,13 +83,13 @@ void calc_gradient_mag(Mat &left, Mat &right, Mat &out) {
             double result = sqrt( pow((int)left.at<double>(y,x),2) + (int)pow(right.at<double>(y,x),2) );
             unNormalised.at<double>(y,x) = result;
         }
-		}
-		normalize(unNormalised, out);
+	}
+	normalize(unNormalised, out);
 }
 
 void calc_gradient_dir(Mat &left, Mat &right, Mat &out)  {
     out.create(left.size(), CV_8U);
-		Mat unNormalised; 
+		Mat unNormalised;
 		left.copyTo(unNormalised);
 
     for (int y = 0; y < left.rows; y++) {
@@ -95,7 +98,7 @@ void calc_gradient_dir(Mat &left, Mat &right, Mat &out)  {
             unNormalised.at<double>(y,x) = result;
         }
     }
-		normalize(unNormalised, out);
+	normalize(unNormalised, out);
 }
 
 void imageToDouble(Mat &input, Mat &out)  {
@@ -123,34 +126,43 @@ void sobel(Mat &image, Mat &gradient_mag, Mat &gradient_dir) {
 
     Mat outputX, outputY, doubleImage, blurredImage;
 
-		imageToDouble(image, doubleImage);
-		normalize(doubleImage, image);
+	imageToDouble(image, doubleImage);
+	normalize(doubleImage, image);
     imwrite("test.jpg", image);
 
 
     convolution(doubleImage, kernelG, 3, blurredImage);
     convolution(blurredImage, kernelX, 3, outputX);
-		Mat uX, uY;
-		normalize(outputX, uX);
+	Mat uX, uY;
+	normalize(outputX, uX);
     imwrite("outputX.jpg", uX);
     convolution(blurredImage, kernelY, 3, outputY);
-		normalize(outputY, uY);
-    imwrite("outputY.jpg", uY);    
+	normalize(outputY, uY);
+    imwrite("outputY.jpg", uY);
     calc_gradient_mag(outputX, outputY, gradient_mag);
-    imwrite("outputMag.jpg", gradient_mag);
     calc_gradient_dir(outputX, outputY, gradient_dir);
-    imwrite("outputDir.jpg", gradient_dir);
 }
 
 void houghTransform(Mat &image, Mat &gradient_mag, Mat &gradient_dir, double thresh_val)  {
-		Mat thresh_mag, thresh_dir;
-		threshold(gradient_mag, thresh_mag, thresh_val, 255, THRESH_BINARY);
-		threshold(gradient_dir, thresh_dir, thresh_val, 255, THRESH_BINARY);
-    imwrite("threshMag.jpg", thresh_mag);
-    imwrite("threshDir.jpg", thresh_dir);
-
-
-
+    Mat gradient_thresh, double_mag, double_dir;
+    imageToDouble(gradient_mag, double_mag);
+    imageToDouble(gradient_dir, double_dir);
+    Mat new_gradient = double_mag.mul(double_dir);
+    int min = round(sqrt(sqr(image.cols) + sqr(image.rows)));
+    Mat hough_space(Size(2 * min, 180), CV_64F, Scalar(0));
+    for (int y = 0; y < new_gradient.rows; y++) {
+        for (int x = 0; x < new_gradient.cols; x++) {
+            if (new_gradient.at<double>(y, x) > 128) {
+                for (double t = 0; t < hough_space.rows; t++) {
+                    int p = round(x * cos(t * M_PI / 180) + y * sin(t * M_PI / 180)) + min;
+                    hough_space.at<double>(t, p) = hough_space.at<double>(t, p) + 1;
+                }
+            }
+        }
+    }
+    Mat hough_out;
+    normalize(hough_space, hough_out);
+    imwrite("houghSpace.jpg", hough_out);
 }
 
 int main(int argc, char** argv) {
@@ -163,9 +175,9 @@ int main(int argc, char** argv) {
         printf(" No image data \n ");
         return -1;
     }
-		Mat grad_mag, grad_dir;
+	Mat grad_mag, grad_dir;
     sobel(image, grad_mag, grad_dir);
-		houghTransform(image, grad_mag, grad_dir, 60);
+	houghTransform(image, grad_mag, grad_dir, 230);
 
     return 0;
 }
