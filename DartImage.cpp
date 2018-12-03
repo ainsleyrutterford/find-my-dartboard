9221bff04db703fc8d62bd27278ee45670a9814f
Harry Waugh <hw16471@my.bristol.ac.uk>
2018-12-03 09:26:10 +0000

    Biig 'ol refactor with new DartImage classes

--- DartImage.cpp
+++ DartImage.cpp
@@ -57,6 +57,7 @@ class DartImage {
             cascade.detectMultiScale(equalised, detected_rects, 
                                      1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500));
             this->detected_rects = detected_rects;
+            printf("detectedRects %lu\n", detected_rects.size());
         }
         void setFilteredRects(vector<Rect> filtered_rects)  {
             this->filtered_rects = filtered_rects;
